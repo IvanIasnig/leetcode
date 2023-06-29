7290c4c5f5558cecd7635be4bd1b7f0740f1a793
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-06-29 23:34:16 +0200

    ciao

--- twoPointers_easy/lexicographically.js
+++ twoPointers_easy/lexicographically.js
@@ -67,3 +67,21 @@ console.log(makeSmallestPalindrome("abcd"));
 
 // if (alfa.c){return alfa.c}
 // else {return false}
+
+
+//---------------------leetcode
+// const makeSmallestPalindrome = function (s) {
+//     const n = s.length
+//     s = s.split('')
+//     const halfN = Math.trunc(n / 2)
+//     for (let i = 0; i < halfN; i++) {
+//       if (s[i] !== s[n - 1 - i]) {
+//         if (s[i] < s[n - 1 - i]) {
+//           s[n - 1 - i] = s[i]
+//         } else {
+//           s[i] = s[n - 1 - i]
+//         }
+//       }
+//     }
+//     return s.join('')
+//   }
