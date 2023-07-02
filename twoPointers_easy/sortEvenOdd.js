e8ad89c1b6ebb5b3f25168ee1aa3132163b9a5fc
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-07-03 01:39:40 +0200

    ciao

--- twoPointers_easy/sortEvenOdd.js
+++ twoPointers_easy/sortEvenOdd.js
@@ -2,31 +2,54 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
+function sortArrayByParity(nums) {
+    let start = 0;
+    let end = nums.length - 1;
+
+    while (start < end) {
+        if (nums[start] % 2 > nums[end] % 2) {
+            let tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+        }
+
+        if (nums[start] % 2 === 0) start++;
+        if (nums[end] % 2 === 1) end--;
+    }
+
+    return nums;
+}
+
+
+// console.log(sortArrayByParity([3, 1, 2, 4]));
+// console.log(sortArrayByParity([2, 1, 3, 4]));
+// console.log(sortArrayByParity([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]));
+console.log(sortArrayByParity([2, 4, 8, 10, 11, 12]));
+console.log(sortArrayByParity([3, 1, 2, 4]));
+console.log(sortArrayByParity([2, 3, 4, 5, 6, 7]));
+console.log(sortArrayByParity([2,4,5,6,8,9]))
+
+//soluzione brutta
+/*
 var sortArrayByParity = function (nums) {
-  start = 0;
-  end = nums.length - 1;
   ris = [];
-  while (start <= end) {
+  let p = [];
+  let d = [];
+  let k = 0;
+  let j = 0;
+  for (i = 0; i < nums.length; i++) {
 
-    if (nums[start] % 2 === 1) {
-      ris[end] = nums[start];
+    if (nums[i] % 2 === 0){
+        p[k] = nums[i]
+        k++
     } else {
-      ris[start] = nums[start];
+        d[j] = nums[i]
+        j++
     }
-
-    if (nums[end] % 2 === 0) {
-      ris[start] = nums[end];
-    } else {
-      ris[end] = nums[end];
-    }
-    start++;
-    end--;
   }
-  return ris;
+  ris = p.concat(d)
+  return  ris;
 };
 
-console.log(sortArrayByParity([3, 1, 2, 4]));
-
-console.log(sortArrayByParity([2, 1, 3, 4]));
-
-console.log(sortArrayByParity([2,3,4,5,6]));
+*/
