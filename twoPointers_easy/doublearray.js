19086795164d6fcc28b46879f9ec4385b867673a
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-07-03 02:34:49 +0200

    ciao

--- twoPointers_easy/doublearray.js
+++ twoPointers_easy/doublearray.js
@@ -9,17 +9,18 @@ var mergeArrays = function (nums1, nums2) {
   let cicli = nums1.length < nums2.length ? nums1.length : nums2.length;
   let ris = [];
   for (i = 0; i < cicli - 1; i++) {
-   if(nums1[0][0] === i && nums2[0][0] === i){
-    ris[0] = []
-    ris[[0][1]]=nums1[2][1] + nums2[2][1]
-    return ris
-   } else {}
+    if (nums1[0][0] === i && nums2[0][0] === i) {
+      ris.push([nums1[i][0], nums1[i][1] + nums2[i][1]]);
+      return ris;
+    } else {
+    }
   }
 };
 
 console.log(
   mergeArrays(
-    [[0, 3],
+    [
+      [0, 3],
       [2, 4],
       [3, 6],
       [4, 3],
