488d9d2033f9f72d1f5842247d04b8b0540b6602
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-07-22 23:16:34 +0200

    linked list

---
+++ prove_varie/linked_list2.js
@@ -0,0 +1,21 @@
+// const n1 = {
+//     data: 100
+// }
+
+// const n2 = {
+//     data: 100
+// }
+
+// n1.next = n2
+
+// console.log(n1);
+
+class Node {
+    constructor(data, next = null){
+        this.data = data;
+        this.next = next;
+    }
+}
+
+const n1 = new Node(100);
+console.log(n1);
