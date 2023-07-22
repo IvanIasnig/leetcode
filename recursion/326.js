c622d7cab74e4e099b9e95130ba21b680db20753
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-07-22 11:08:11 +0200

    326

---
+++ recursion/326.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    if(n===1){
+        return true
+    }
+    if(n%3 != 0 || n === 0){
+        return false
+    }
+    return isPowerOfThree(n/3)
+};
+
+console.log(isPowerOfThree(27))
