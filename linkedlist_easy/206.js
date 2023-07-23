f067c33835cef9d71401fb0ee62382bba5f63c9a
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-07-24 00:54:13 +0200

    206 finitio

--- linkedlist_easy/206.js
+++ linkedlist_easy/206.js
@@ -15,12 +15,24 @@ node3.next = node4;
 node4.next = node5;
 //node5.next = node3;
 
-let head = node1;
+// Creare i nodi come hai fatto tu
+// let node1 = new ListNode(100);
+// let node2 = new ListNode(200, node1);
+// let node3 = new ListNode(300, node2);
+// let node4 = new ListNode(400, node3);
+// let node5 = new ListNode(500, node4);
 
-while (head !== null) {
-  console.log(head.val);
-  head = head.next;
+// Funzione per stampare la lista collegata
+function printLinkedList(head) {
+  let currentNode = head;
+  while (currentNode != null) {
+    console.log(currentNode.val);
+    currentNode = currentNode.next;
+  }
 }
+
+// Chiamata della funzione per stampare la lista a partire da node5
+printLinkedList(node1);
 
 // let i = 0;
 // while (i < 12) {
@@ -29,9 +41,18 @@ while (head !== null) {
 //   i++;
 // }
 
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
+var reverseList = function (head) {
+  let prev = null;
+  let next = null;
+  while (head) {
+    next = head.next;
+    head.next = prev;
+    prev = head;
+    head = next;
+  }
+  return prev;
+};
 
-var reverseList = function (head) {};
+
+console.log("***********")
+console.log(reverseList(node1))
