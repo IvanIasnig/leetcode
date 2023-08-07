f2b2ff38afc5fac980895e5534caed7c39685dd4
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-08-07 17:09:32 +0200

    tree

--- prove_varie/provabinary.js
+++ prove_varie/provabinary.js
@@ -17,53 +17,36 @@ node2.left = node4;
 node2.right = node5;
 node3.right = node6;
 
-// var depthFirstValues = (head) => {
-//   if (root === null) return [];
-
-//   let stack = [head];
-//   let result = [];
-//   while (stack.length > 0) {
-//     const current = stack.pop();
-//     result.push(current.val);
-//     if (current.left) stack.push(current.left);
-//     if (current.right) stack.push(current.right);
-//   }
-//   return result;
-// };
-
-// console.dir("-------------");
-// console.dir(depthFirstValues(node1), { depth: null });
-
-// Inizializzazione:
-
-// Se il nodo di partenza head è null, restituisci un array vuoto, poiché non c'è nulla da esplorare.
-// Inizializza uno stack con il nodo di partenza head. Gli stack sono particolarmente utili per le visite in profondità poiché ti permettono di esplorare una "strada" dell'albero fino alla fine prima di tornare indietro.
-// Crea un array result vuoto, in cui verranno memorizzati i valori dei nodi visitati nell'ordine in cui vengono visitati.
-// Iterazione:
-
-// Mentre lo stack non è vuoto (cioè ci sono ancora nodi da esplorare), ripeti i seguenti passi:
-// Rimuovi l'ultimo nodo dallo stack (con pop()) e assegnalo a una variabile current. Questo nodo rappresenta il nodo corrente che stai esplorando.
-// Aggiungi il valore del nodo corrente all'array result (con push()).
-// Se il nodo corrente ha un figlio sinistro, aggiungilo allo stack.
-// Se il nodo corrente ha un figlio destro, aggiungilo allo stack.
-// Risultato:
-
-// Una volta che tutti i nodi sono stati esplorati e lo stack è vuoto, restituisci l'array result che contiene i valori di tutti i nodi nell'ordine in cui sono stati visitati.
-
-//TODO: da fare recursive
-var recDepthFirstValues = (head) => {
-  if (head === null) return [];
+var depthFirstValues = (head) => {
+  if (root === null) return [];
 
   let stack = [head];
   let result = [];
   while (stack.length > 0) {
-    console.log(stack);
     const current = stack.pop();
     result.push(current.val);
     if (current.left) stack.push(current.left);
     if (current.right) stack.push(current.right);
   }
   return result;
+};
+
+// console.dir("-------------");
+// console.dir(depthFirstValues(node1), { depth: null });
+
+/*
+All'inizio creato un array che mi servirà per creare e salvare i vari nodi,
+l'array result servirà solo per pushare all'interno i val dei nodi.
+current mi serve per estrarre l'ultimo nodo dall'array (in questo caso per come sono ordinati gli "if" sarà il nodo right)
+una volta che tutti i nodi right saranno stati estratti il push avverrà solo sui nodi left
+-> output -> [1,3,6,2,5,4]
+*/
+
+var recDepthFirstValues = (head) => {
+  if (head === null) return [];
+  const leftValues = depthFirstValues(head.left);
+  const rightValues = depthFirstValues(head.right);
+  return [head.val, ...leftValues, rightValues];
 };
 
 console.dir(recDepthFirstValues(node1), { depth: null });
